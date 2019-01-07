c0db332ea4529525181887cd478714d5c2165224
Trevor Hegert <thegert138@gmail.com>
2019-01-07 15:38:43 -0500

    Pig Game v1.0.0 Done

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,16 +11,24 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+function newGame() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.btn-roll').style.display = 'block';
+    document.querySelector('.btn-hold').style.display = 'block';
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+};
 
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+newGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
@@ -72,4 +80,6 @@ function endTurn() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 };
 
-// var x = document.querySelector('#score-0').textContent;
+document.querySelector('.btn-new').addEventListener('click', function () {
+    newGame();
+})
