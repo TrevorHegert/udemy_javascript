40b31ff230e8f6af33cf90d5d970557eb6d8470a
Trevor Hegert <thegert138@gmail.com>
2019-01-09 16:52:41 -0500

    Budgety 80 - Initialization Function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -12,7 +12,7 @@ UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value'
-    }
+    };
 
     return {
         getInput: function () {
@@ -20,7 +20,7 @@ UIController = (function () {
                 type: document.querySelector(DOMstrings.inputType).value, //Will be inc/exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value,
-            }
+            };
         },
 
         getDOMstrings: function () {
@@ -33,12 +33,20 @@ UIController = (function () {
 //GLOBAL APP CONTROLLER
 controller = (function (budgetCtrl, UICtrl) {
 
-    DOM = UICtrl.getDOMstrings();
+    setupEventListeners = function () {
+        DOM = UICtrl.getDOMstrings();
+        document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-    ctrlAddItem = () => {
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    ctrlAddItem = function () {
         //Get Input Data from Field
         input = UICtrl.getInput();
-        console.log(input);
 
         //Add item to the budget controller
 
@@ -50,11 +58,13 @@ controller = (function (budgetCtrl, UICtrl) {
 
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-        event.keyCode === 13 ? ctrlAddItem() : null;
-
-    });
+    return {
+        init: function () {
+            console.log('Application Initiated');
+            setupEventListeners();
+        }
+    }
 
 })(budgetController, UIController);
+
+controller.init();
