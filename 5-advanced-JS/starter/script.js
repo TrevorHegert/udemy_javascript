b4b7fd1ec916ec3298c4548ab0c99d76400dd1fa
Trevor Hegert <thegert138@gmail.com>
2019-01-09 11:58:29 -0500

    Section 5 Lesson 63

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,32 +1,59 @@
 //Function Constructor - Capitalize their variables
 
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
+// var john = {
+//     name: 'John',
+//     yearOfBirth: 1990,
+//     job: 'teacher'
+// };
+
+// var Person = function (name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+// Person.prototype.calculateAge = function () {
+//     console.log(2018 - this.yearOfBirth);
+// }
+
+// Person.prototype.lastName = 'Smith';
+
+// var john = new Person('John', 1990, 'teacher');
+// var jane = new Person('Jane', 1969, 'designer');
+// var mark = new Person('Mark', 1948, 'retired');
+
+// mark.calculateAge();
+// john.calculateAge();
+// jane.calculateAge();
+
+
+// console.log(john.lastName);
+// console.log(mark.lastName);
+// console.log(jane.lastName);
+
+
+//Object.create Method
+
+var personProto = {
+    calculateAge: function () {
+        console.log(2016 - yearOfBirth);
+    }
 };
 
-var Person = function (name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-}
+var john = Object.create(personProto);
 
-Person.prototype.calculateAge = function () {
-    console.log(2018 - this.yearOfBirth);
-}
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-Person.prototype.lastName = 'Smith';
-
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-mark.calculateAge();
-john.calculateAge();
-jane.calculateAge();
-
-
-console.log(john.lastName);
-console.log(mark.lastName);
-console.log(jane.lastName);
+var jane = Object.create(personProto, {
+    name: {
+        value: 'Jane'
+    },
+    yearOfBirth: {
+        value: 1969
+    },
+    job: {
+        value: 'designer'
+    }
+});
