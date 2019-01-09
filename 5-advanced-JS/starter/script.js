93aa798f4e236f9ed6a30a506f32f70ecffef1e8
Trevor Hegert <thegert138@gmail.com>
2019-01-09 12:30:42 -0500

    Section 5 Lesson 64

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -34,26 +34,62 @@
 
 //Object.create Method
 
-var personProto = {
-    calculateAge: function () {
-        console.log(2016 - yearOfBirth);
-    }
+// var personProto = {
+//     calculateAge: function () {
+//         console.log(2016 - yearOfBirth);
+//     }
+// };
+
+// var john = Object.create(personProto);
+
+// john.name = 'John';
+// john.yearOfBirth = 1990;
+// john.job = 'teacher';
+
+// var jane = Object.create(personProto, {
+//     name: {
+//         value: 'Jane'
+//     },
+//     yearOfBirth: {
+//         value: 1969
+//     },
+//     job: {
+//         value: 'designer'
+//     }
+// });
+
+
+// Primitives vs. Objects
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
 };
 
-var john = Object.create(personProto);
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
 
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'teacher';
-
-var jane = Object.create(personProto, {
-    name: {
-        value: 'Jane'
-    },
-    yearOfBirth: {
-        value: 1969
-    },
-    job: {
-        value: 'designer'
-    }
-});
+change(age, obj);
+console.log(age);
+console.log(obj.city)
