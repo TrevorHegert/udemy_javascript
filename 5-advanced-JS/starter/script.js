a46ec163b2150d21a2fc1b561f7c828fd5cc4e83
Trevor Hegert <thegert138@gmail.com>
2019-01-09 12:49:57 -0500

    Section 5 Lesson 65

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -31,7 +31,7 @@
 // console.log(mark.lastName);
 // console.log(jane.lastName);
 
-
+////////////////////////////////////////////////////////////////
 //Object.create Method
 
 // var personProto = {
@@ -58,38 +58,65 @@
 //     }
 // });
 
+//////////////////////////////////////////////////////////////////
+// // Primitives vs. Objects
 
-// Primitives vs. Objects
+// //Primitives
+// var a = 23;
+// var b = a;
+// a = 46;
+// console.log(a);
+// console.log(b);
 
-//Primitives
-var a = 23;
-var b = a;
-a = 46;
-console.log(a);
-console.log(b);
+// // Objects
+// var obj1 = {
+//     name: 'John',
+//     age: 26
+// };
+// var obj2 = obj1;
+// obj1.age = 30;
+// console.log(obj1.age);
+// console.log(obj2.age);
 
-// Objects
-var obj1 = {
-    name: 'John',
-    age: 26
+// // Functions
+// var age = 27;
+// var obj = {
+//     name: 'Jonas',
+//     city: 'Lisbon'
+// };
+
+// function change(a, b) {
+//     a = 30;
+//     b.city = 'San Francisco';
+// }
+
+// change(age, obj);
+// console.log(age);
+// console.log(obj.city)
+
+///////////////////////////////////////////////////////////////
+// Passing Functions as Arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (i = 0; 1 < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    };
+    return arrRes;
 };
-var obj2 = obj1;
-obj1.age = 30;
-console.log(obj1.age);
-console.log(obj2.age);
 
-// Functions
-var age = 27;
-var obj = {
-    name: 'Jonas',
-    city: 'Lisbon'
+function calculateAge(el) {
+    return 2016 - el;
 };
 
-function change(a, b) {
-    a = 30;
-    b.city = 'San Francisco';
+function isAdult(el) {
+    return el >= 18;
 }
 
-change(age, obj);
-console.log(age);
-console.log(obj.city)
+// var ages = arrayCalc(years, calculateAge);
+// // console.log(ages);
+
+// var adults = arrayCalc(years, isAdult);
+// console.log(adults);
