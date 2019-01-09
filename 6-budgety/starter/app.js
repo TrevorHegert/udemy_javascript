697abccabc229c15d11a9aaa5222ac6dd143e934
Trevor Hegert <thegert138@gmail.com>
2019-01-09 16:41:04 -0500

    Budgety 79 - Input Data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -8,16 +8,37 @@ budgetController = (function () {
 //UI CONTROLLER
 UIController = (function () {
 
-    //SOME CODE
+    DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    }
 
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be inc/exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
 
 })();
 
 //GLOBAL APP CONTROLLER
 controller = (function (budgetCtrl, UICtrl) {
 
+    DOM = UICtrl.getDOMstrings();
+
     ctrlAddItem = () => {
         //Get Input Data from Field
+        input = UICtrl.getInput();
+        console.log(input);
 
         //Add item to the budget controller
 
@@ -27,13 +48,12 @@ controller = (function (budgetCtrl, UICtrl) {
 
         //Display the budget on UI
 
-        console.log('It Works!')
     };
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
-        ctrlAddItem();
+        event.keyCode === 13 ? ctrlAddItem() : null;
 
     });
 
