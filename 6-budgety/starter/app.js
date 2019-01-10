c7863778e25ca59a06dadaa01a9a0214f5f00270
Trevor Hegert <thegert138@gmail.com>
2019-01-10 13:22:38 -0500

    79 Done

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,12 +1,53 @@
 //////////////////////////////////////////////////
+// Budget Calculation Module
+
+var budgetController = (function () {
+
+
+})();
+
+
+//////////////////////////////////////////////////
+// UI Interface Module
+
+var UIController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+})();
+
+
+
+//////////////////////////////////////////////////
 // Command Module
 
-var controller = (function (budgtCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function () {
-        console.log('It works!');
-        //1. Get the user input
 
+        //1. Get the user input
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. Handoff item to budget controller
 
         //3. Add new item to UI
@@ -16,38 +57,12 @@ var controller = (function (budgtCtrl, UICtrl) {
         //5. Display Budget to UI 
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
         }
     });
+
 })(budgetController, UIController);
-
-
-
-
-
-
-
-//////////////////////////////////////////////////
-// UI Interface Module
-
-var UIController = (function () {
-
-});
-
-
-
-
-
-
-
-//////////////////////////////////////////////////
-// Budget Calculation Module
-
-var budgetController = (function () {
-
-
-});
