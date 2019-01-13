d08e256f71f877acb84dba8ea2184d8410d5aa8e
Trevor Hegert <thegert138@gmail.com>
2019-01-12 19:16:55 -0500

    Reset UI data to 0 upon init function added

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -226,6 +226,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log("Application started.");
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
