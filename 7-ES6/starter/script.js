49df42ef7801965b8a38ba4f492c74a61f424cf6
Trevor Hegert <thegert138@gmail.com>
2019-01-14 16:48:06 -0500

    ES6  Arrow Functions

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -41,3 +41,34 @@ console.log(n.startsWith('j'));
 console.log(n.endsWith('th'));
 console.log(n.includes(' '));
 console.log(`${firstName}, `.repeat(5));
+
+
+////////////////////////////////////
+//Arrow Functions
+
+const years = [1990, 1965, 1982, 1937];
+
+//ES5
+var ages5 = years.map(function (el) {
+    return 2015 - el;
+});
+console.log(ages5);
+
+
+//ES6
+
+//(), {}, and return not necessary with simple functions with only 1 argument
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+//If multiple arguments are needed the () around them are necessary
+ages6 = years.map((el, index) => `Age Element ${index+1}: ${2018 - el}.`);
+console.log(ages6);
+
+//If multiple lines are needed for function {} and return become necessary
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `For the year ${now}, Age Element ${index+1}: ${age}.`
+});
+console.log(ages6);
