bdc7de919eb851f12040346dc14ec8db33b8eecf
Trevor Hegert <thegert138@gmail.com>
2019-01-14 16:24:57 -0500

    ES6  Blocks and IFFEs

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,17 @@
+/////////////////////////////////
+//Blocks and IIFEs
+
+//ES6
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+console.log(c);
+
+
+//ES5
+(function () {
+    var c = 3;
+})();
