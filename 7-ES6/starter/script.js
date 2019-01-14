76282f9e92cd51f9a220d8f52d83d10adb3ba47f
Trevor Hegert <thegert138@gmail.com>
2019-01-14 16:35:37 -0500

    ES6  Strings and Template Literals

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -15,3 +15,29 @@ console.log(c);
 (function () {
     var c = 3;
 })();
+
+
+/////////////////////////////////
+//Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2016 - year;
+}
+
+//ES5
+
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today he is ' + calcAge(yearOfBirth) + ' years old.');
+
+//ES6
+
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)}.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('th'));
+console.log(n.includes(' '));
+console.log(`${firstName}, `.repeat(5));
