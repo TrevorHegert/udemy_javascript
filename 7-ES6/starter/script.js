117155875b6a27673ffc6c7ddbea2d928850face
Trevor Hegert <thegert138@gmail.com>
2019-01-15 12:40:31 -0500

    ES6  Arrow Functions 3

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -125,13 +125,19 @@ function Person(name) {
 //ES5
 Person.prototype.myFriends5 = function (friends) {
     var arr = friends.map(function (el) {
-        var self = this;
-        var friendList = self.name + ' is friends with ' + el;
-        return friendList;
-    });
+        return this.name + ' is friends with ' + el;
+    }.bind(this));
 
     console.log(arr);
 };
 
 var friends = ['Bob', 'Jane', 'Mark'];
 new Person('John').myFriends5(friends);
+
+//ES6
+Person.prototype.myFriends5 = function (friends) {
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr);
+};
+
+new Person('Mike').myFriends5(friends);
