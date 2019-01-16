7a591b11172f5edde907e88371c73403397e717f
Trevor Hegert <thegert138@gmail.com>
2019-01-16 11:56:34 -0500

    Destructuring

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -3,45 +3,52 @@
 
 //ES6
 {
-    const a = 1;
-    let b = 2;
-    var c = 3;
+  const a = 1;
+  let b = 2;
+  var c = 3;
 }
 
 console.log(c);
 
-
 //ES5
-(function () {
-    var c = 3;
+(function() {
+  var c = 3;
 })();
-
 
 /////////////////////////////////
 //Strings
 
-let firstName = 'John';
-let lastName = 'Smith';
+let firstName = "John";
+let lastName = "Smith";
 const yearOfBirth = 1990;
 
 function calcAge(year) {
-    return 2016 - year;
+  return 2016 - year;
 }
 
 //ES5
 
-console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today he is ' + calcAge(yearOfBirth) + ' years old.');
+console.log(
+  "This is " +
+    firstName +
+    " " +
+    lastName +
+    ". He was born in " +
+    yearOfBirth +
+    ". Today he is " +
+    calcAge(yearOfBirth) +
+    " years old."
+);
 
 //ES6
 
 console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)}.`);
 
 const n = `${firstName} ${lastName}`;
-console.log(n.startsWith('j'));
-console.log(n.endsWith('th'));
-console.log(n.includes(' '));
+console.log(n.startsWith("j"));
+console.log(n.endsWith("th"));
+console.log(n.includes(" "));
 console.log(`${firstName}, `.repeat(5));
-
 
 ////////////////////////////////////
 //Arrow Functions
@@ -49,11 +56,10 @@ console.log(`${firstName}, `.repeat(5));
 const years = [1990, 1965, 1982, 1937];
 
 //ES5
-var ages5 = years.map(function (el) {
-    return 2015 - el;
+var ages5 = years.map(function(el) {
+  return 2015 - el;
 });
 console.log(ages5);
-
 
 //ES6
 
@@ -62,14 +68,14 @@ let ages6 = years.map(el => 2016 - el);
 console.log(ages6);
 
 //If multiple arguments are needed the () around them are necessary
-ages6 = years.map((el, index) => `Age Element ${index+1}: ${2018 - el}.`);
+ages6 = years.map((el, index) => `Age Element ${index + 1}: ${2018 - el}.`);
 console.log(ages6);
 
 //If multiple lines are needed for function {} and return become necessary
 ages6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    const age = now - el;
-    return `For the year ${now}, Age Element ${index+1}: ${age}.`
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `For the year ${now}, Age Element ${index + 1}: ${age}.`;
 });
 console.log(ages6);
 
@@ -78,29 +84,28 @@ console.log(ages6);
 
 //ES5 - this keyword not available outside the function (self hack)
 var box5 = {
-    color: 'green',
-    position: 1,
-    clickMe: function () {
-        var self = this;
-        document.querySelector('.green').addEventListener('click', function () {
-            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
-            alert(str);
-        });
-    }
+  color: "green",
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector(".green").addEventListener("click", function() {
+      var str = "This is box number " + self.position + " and it is " + self.color;
+      alert(str);
+    });
+  }
 };
 box5.clickMe();
 
-
 //ES6 - Arrow functions preserve the this keyword
 const box6 = {
-    color: 'green',
-    position: 1,
-    clickMe: function () {
-        document.querySelector('.green').addEventListener('click', () => {
-            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
-            alert(str);
-        });
-    }
+  color: "green",
+  position: 1,
+  clickMe: function() {
+    document.querySelector(".green").addEventListener("click", () => {
+      var str = "This is box number " + this.position + " and it is " + this.color;
+      alert(str);
+    });
+  }
 };
 box6.clickMe();
 
@@ -119,25 +124,62 @@ box6.clickMe();
 // box66.clickMe();
 
 function Person(name) {
-    this.name = name;
+  this.name = name;
 }
 
 //ES5
-Person.prototype.myFriends5 = function (friends) {
-    var arr = friends.map(function (el) {
-        return this.name + ' is friends with ' + el;
-    }.bind(this));
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(
+    function(el) {
+      return this.name + " is friends with " + el;
+    }.bind(this)
+  );
 
-    console.log(arr);
+  console.log(arr);
 };
 
-var friends = ['Bob', 'Jane', 'Mark'];
-new Person('John').myFriends5(friends);
+var friends = ["Bob", "Jane", "Mark"];
+new Person("John").myFriends5(friends);
 
 //ES6
-Person.prototype.myFriends5 = function (friends) {
-    var arr = friends.map(el => `${this.name} is friends with ${el}`);
-    console.log(arr);
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(el => `${this.name} is friends with ${el}`);
+  console.log(arr);
 };
 
-new Person('Mike').myFriends5(friends);
+new Person("Mike").myFriends5(friends);
+
+////////////////////////////////////////////////////////////
+//Destructuring
+
+//ES5
+// var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
+
+//ES6
+const [name, age] = ["John", 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+  nameFirst: "John",
+  nameLast: "Smith"
+};
+
+const { nameFirst, nameLast } = obj;
+console.log(nameFirst);
+console.log(nameLast);
+
+const { nameFirst: a, nameLast: b } = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+  const age = new Date().getFullYear() - year;
+  return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
